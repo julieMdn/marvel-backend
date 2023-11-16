f42cfc373a7f9ca79663aa9fbf7811e1ffa8d7c4
julieMdn <julie.midon@gmail.com>
2023-11-16 16:06:00 +0100

    routes added

--- index.js
+++ index.js
@@ -29,6 +29,29 @@ app.get("/comics", async (req, res) => {
   }
 });
 
+//Get all informations of specific comic
+app.get("/comic/:comicId", async (req, res) => {
+  try {
+    const apiKey = process.env.API_KEY;
+    const comicId = req.params.comicId;
+
+    if (!apiKey) {
+      return res.status(400).json({ error: "API key is required." });
+    }
+
+    const url = `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${apiKey}`;
+
+    const response = await axios.get(url);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Erreur lors de la requête vers l'API du comic:", error);
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la requête vers l'API du comic." });
+  }
+});
+
 // Get a list of characters
 app.get("/characters", async (req, res) => {
   try {
@@ -51,6 +74,32 @@ app.get("/characters", async (req, res) => {
     res
       .status(500)
       .json({ error: "Erreur lors de la requête vers l'API de personnages." });
+  }
+});
+
+// Get a list of comics containing a specific character
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    const apiKey = process.env.API_KEY;
+    const characterId = req.params.characterId;
+
+    if (!apiKey) {
+      return res.status(400).json({ error: "API key is required." });
+    }
+
+    const url = `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${apiKey}`;
+
+    const response = await axios.get(url);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(
+      "Erreur lors de la requête vers l'API des comics par personnage:",
+      error
+    );
+    res.status(500).json({
+      error: "Erreur lors de la requête vers l'API des comics par personnage.",
+    });
   }
 });
 
